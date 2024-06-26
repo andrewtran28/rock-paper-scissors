1aa26fd246c3c70dbd22a9e5bab4fa22032235ba
andrewtran28 <tran.andrew@outlook.com>
2024-06-26 08:19:16 -0400

    Implement game functionality with scores.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-console.log("This is the Rock Paper Scissors JavaScript.");
-
 //Global Variables
 let humanScore = 0, computerScore = 0;
+
+//cheat = 1 lets you see the computers choice before making a decision.
+let cheat = 1;
 
 function getComputerChoice() {
 
@@ -11,50 +12,103 @@ function getComputerChoice() {
 
     //Converts number to shape string
     if (computerNum === 0) {
-        computerChoice = "rock";
+        computerChoice = "ROCK";
     }
     
     else if (computerNum === 1) {
-        computerChoice = "paper";
+        computerChoice = "PAPER";
     }
 
     else if (computerNum === 2) {
-        computerChoice = "scissors";
+        computerChoice = "SCISSORS";
     }
 
     else {
-        computerChoice = "Not valid shape";
+        computerChoice = "Not a valid shape";
     }
 
-    // console.log("The computer chose: " + computerChoice);
+    if (cheat == 1) console.log("[Cheat] The computer chose " + computerChoice);
     return computerChoice;
 }
 
 function getHumanChoice() {
-    let humanChoice = window.prompt("Choose 'rock', 'paper', or 'scissors'.");
+    let humanChoice = window.prompt("Choose rock, paper, or scissors.").toUpperCase();
 
-    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
-         humanChoice = window.prompt("Choice is invalid. Please type 'rock', 'paper', or 'scissors'.");
+    while (humanChoice != "ROCK" && humanChoice != "PAPER" && humanChoice != "SCISSORS") {
+         humanChoice = window.prompt("Choice is invalid. Please type 'rock', 'paper', or 'scissors'.").toUpperCase();
     }
 
-    console.log("You have chosen: " + humanChoice);
     return humanChoice;
 }
 
-function playRound () {
-    let computerChoice = getComputerChoice();
+function playRound (computerChoice, humanChoice) {
     console.log("Computer chose: " + computerChoice);
-    let humanChoice = getHumanChoice();
-    console.log("You have chose: " + humanChoice);
+    console.log("You have chosen: " + humanChoice);
 
-
-    if (computerChoice === humanChoice) {
-
-        console.log("It is a tie!");
+    if (humanChoice == computerChoice) {
+        console.log("It is a tie! Both players chose " + humanChoice + "!");
     }
 
+    else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
+        console.log("You lose... " + humanChoice + " beats " + computerChoice + "!");
+        computerScore++;
+    }
+
+    else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + "!");
+        humanScore++;
+    }
+
+    else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + "!");
+        humanScore++;
+    }
+
+    else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+        console.log("You lose... " + humanChoice + " beats " + computerChoice + "!");
+        computerScore++;
+    }
+
+    else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
+        console.log("You lose... " + humanChoice + " beats " + computerChoice + "!");
+        computerScore++;
+    }
+
+    else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + "!");
+        humanScore++;
+    }
+
+    else {
+        console.log("Something went wrong with the game, restarting round...");
+    }
 }
 
-console.log("Game started.");
-getHumanChoice();
-// playRound();
+function showScore() {
+    console.log("Score: " + humanScore + " (You) - " + computerScore);
+}
+
+function startGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    showScore();
+    while (humanScore <3 && computerScore <3) {
+        playRound(getComputerChoice(), getHumanChoice());
+        showScore();
+    }
+
+    if (humanScore >= 3 && computerScore <3) {
+        console.log("Congratulations! You win the game! Rematching in 5 seconds...");
+    }
+
+    else if (computerScore >= 3 && humanScore <3) {
+        console.log("You lost the game... Rematching in 5 seconds...");
+    }
+
+    setTimeout(() => { startGame(); }, 5000);
+}
+
+console.log("This is the Rock Paper Scissors Game!");
+startGame();
+
