b0d25738c2c4c86c88e9ca099ed0243308c40923
andrewtran28 <tran.andrew@outlook.com>
2024-06-26 07:04:09 -0400

    Test

--- scripts.js
+++ scripts.js
@@ -1,21 +1,60 @@
 console.log("This is the Rock Paper Scissors JavaScript.");
 
+//Global Variables
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
-    // Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-    // Need number between 0-2
 
-    let computerChoice = Math.floor(Math.random()*3);
-    // Chooses a number between 0 and 2;
+    // Chooses a number between 1 and 3
+    let computerChoice;
+    let computerNum = Math.floor(Math.random()*3);
 
-    console.log("The computer chose: " + computerChoice);
+    //Converts number to shape string
+    if (computerNum === 0) {
+        computerChoice = "rock";
+    }
+    
+    else if (computerNum === 1) {
+        computerChoice = "paper";
+    }
 
+    else if (computerNum === 2) {
+        computerChoice = "scissors";
+    }
+
+    else {
+        computerChoice = "Not valid shape";
+    }
+
+    // console.log("The computer chose: " + computerChoice);
+    return computerChoice;
 }
 
 function getHumanChoice() {
-    let humanChoice = window.prompt("What is your choice?");
+    let humanChoice = window.prompt("Choose 'rock', 'paper', or 'scissors'.");
+
+    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
+         humanChoice = window.prompt("Choice is invalid. Please type 'rock', 'paper', or 'scissors'.");
+    }
+
     console.log("You have chosen: " + humanChoice);
-    // Need to implement buttons for the human to choose.
+    return humanChoice;
 }
 
-getComputerChoice();
+function playRound () {
+    let computerChoice = getComputerChoice();
+    console.log("Computer chose: " + computerChoice);
+    let humanChoice = getHumanChoice();
+    console.log("You have chose: " + humanChoice);
+
+
+    if (computerChoice === humanChoice) {
+
+        console.log("It is a tie!");
+    }
+
+}
+
+console.log("Game started.");
 getHumanChoice();
+// playRound();
