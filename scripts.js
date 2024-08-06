80f105ebd1afe205636c7ab4783fef6a3c7d634a
andrewtran28 <tran.andrew@outlook.com>
2024-08-06 08:33:16 -0400

    Game functioning

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 //Global Variables
 let humanScore = 0, computerScore = 0;
 let round = 1;
+let computerChoice = "";
 let humanChoice = "";
 let gameScore = document.querySelector(".scoreboard");
 let scoreboard = document.createElement("div");
@@ -10,9 +11,7 @@ let roundScore = document.createElement("div");
 let cheat = 1;
 
 function getComputerChoice() {
-
     // Chooses a number between 1 and 3
-    let computerChoice;
     let computerNum = Math.floor(Math.random()*3);
 
     //Converts number to shape string
@@ -38,75 +37,123 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    //  let humanChoice = window.prompt("Choose rock, paper, or scissors.").toUpperCase();
-    let humanChoice = "";
+    //Need to add playResult after each button
+    let buttons = document.querySelector(".rpsgame");
+    let showButtons = document.createElement("section");
+    let btn_rock = document.createElement("button");
+    let btn_paper = document.createElement("button");
+    let btn_scissors = document.createElement("button");
 
-    // while (humanChoice != "ROCK" && humanChoice != "PAPER" && humanChoice != "SCISSORS") {
-    //      humanChoice = window.prompt("Choice is invalid. Please type 'rock', 'paper', or 'scissors'.").toUpperCase();
-    // }
+    buttons.appendChild(showButtons);
+    showButtons.appendChild(btn_rock);
+    showButtons.appendChild(btn_paper);
+    showButtons.appendChild(btn_scissors);
+    btn_rock.textContent = "Rock";
+    btn_paper.textContent = "Paper";
+    btn_scissors.textContent = "Scissors";
+
+    btn_rock.addEventListener("click", () => {
+        console.log("You chose Rock.");
+        humanChoice = "ROCK";
+        buttons.removeChild(showButtons);
+        playResult();
+    });
+
+    btn_paper.addEventListener("click", () => {
+        console.log("You chose Paper.");
+        humanChoice = "PAPER";
+        buttons.removeChild(showButtons);
+        playResult();
+    });
+
+    btn_scissors.addEventListener("click", () => {
+        console.log("You chose Scissors.");
+        humanChoice = "SCISSORS";
+        buttons.removeChild(showButtons);
+        playResult();
+    });
 
     return humanChoice;
 }
 
-function playRound (computerChoice, humanChoice) {
-    let gameSection = document.querySelector(".result");
+function playRound() {
+    humanChoice = "";
+    computerChoice = "";
+
+    getComputerChoice();
+    getHumanChoice();
+}
+
+function playResult() {
+    let gameSection = document.querySelector(".rpsgame");
     let gameResult = document.createElement("div");
     let roundEnd = document.createElement("button");
     
+    gameSection.appendChild(gameResult);
+
     if (humanChoice == computerChoice) {
         gameResult.textContent = "It is a tie! Both players chose " + humanChoice + "!";
+        roundEnd.textContent = "Restarting Round";
         round++;
     }
 
     else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
         gameResult.textContent = "You lose... " + humanChoice + " beats " + computerChoice + "!";
+        roundEnd.textContent = "Continue";
         computerScore++;
         round++;
     }
 
     else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
         gameResult.textContent = "You win! " + humanChoice + " beats " + computerChoice + "!"
+        roundEnd.textContent = "Continue";
         humanScore++;
         round++;
     }
 
     else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
         gameResult.textContent = "You win! " + humanChoice + " beats " + computerChoice + "!";
+        roundEnd.textContent = "Continue";
         humanScore++;
         round++;
     }
 
     else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
         gameResult.textContent = "You lose... " + humanChoice + " beats " + computerChoice + "!";
+        roundEnd.textContent = "Continue";
         computerScore++;
         round++;
     }
 
     else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
         gameResult.textContent = "You lose... " + humanChoice + " beats " + computerChoice + "!";
+        roundEnd.textContent = "Continue";
         computerScore++;
         round++;
     }
 
     else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
         gameResult.textContent = "You win! " + humanChoice + " beats " + computerChoice + "!";
+        roundEnd.textContent = "Continue";
         humanScore++;
         round++;
     }
 
     else {
         gameResult.textContent = "Something went wrong with the game, restarting round...";
-        gameResult.appendChild(roundEnd);
         roundEnd.textContent = "Restart Round";
-
-        roundEnd.addEventListener("click", () => {
-            gameResult.removeChild(roundEnd);
-        });
-
     }
 
+
+    gameResult.appendChild(roundEnd);
+    roundEnd.addEventListener("click", () => {
+        gameSection.removeChild(gameResult);
+        gameResult.removeChild(roundEnd);
+        playRound();
+    });
+
     showScore();
-    gameSection.appendChild(gameResult);
+    //gameSection.appendChild(gameResult);
     // setTimeout(() => { startGame(); }, 1000);
 }
 
@@ -119,6 +166,9 @@ function startGame() {
 
     let gameStart = document.querySelector(".scoreboard");
     let btn_start = document.createElement("button");
+    humanChoice = "";
+    humanScore = 0;
+    computerScore = 0;
 
     gameStart.appendChild(btn_start);
     btn_start.textContent = "Start Game";
@@ -128,11 +178,8 @@ function startGame() {
         gameScore.appendChild(roundScore);
         gameScore.appendChild(scoreboard);
         showScore();
+        playRound();
     });
-
-    let humanChoice = "";
-    humanScore = 0;
-    computerScore = 0;
 
     //console.log("Game start!");
     //showScore();
@@ -155,31 +202,3 @@ function startGame() {
 
 console.log("This is the Rock Paper Scissors Game!");
 startGame();
-
-
-
-
-
-let btn_rock = document.querySelector("#btn_rock");
-btn_rock.addEventListener("click", () => {
-    console.log("You chose rock");
-
-    humanChoice = "ROCK";
-    playRound(getComputerChoice(), humanChoice);
-});
-
-let btn_paper = document.querySelector("#btn_paper");
-btn_paper.addEventListener("click", () => {
-    console.log("You chose paper");
-
-    humanChoice = "PAPER";
-    playRound(getComputerChoice(), humanChoice);
-});
-
-let btn_scissors = document.querySelector("#btn_scissors");
-btn_scissors.addEventListener("click", () => {
-    console.log("You chose scissors");
-
-    humanChoice = "SCISSORS";
-    playRound(getComputerChoice(), humanChoice);
-});
